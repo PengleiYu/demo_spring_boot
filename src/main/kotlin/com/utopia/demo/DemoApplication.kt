758a5c006d16eefbf91bce2c8b8b05a364fde1c4
yupenglei <yupenglei@kuaishou.com>
2024-09-05 19:26:33 +0800

    实现Hello world

---
+++ src/main/kotlin/com/utopia/demo/DemoApplication.kt
@@ -0,0 +1,18 @@
+package com.utopia.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class DemoApplication {
+    @GetMapping("/hello")
+    fun hello(@RequestParam(value = "name", defaultValue = "World") name: String): String = "Hello $name"
+}
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
